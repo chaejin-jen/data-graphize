e41f6907f7cd6332836d56f5418cb14e0ee32ef8
Chaejin Kim <73324850+chaejin-jen@users.noreply.github.com>
2024-02-02 13:20:58 +0900

    chore: Added stylelint (#15)

---
+++ .stylelintrc.js
@@ -0,0 +1,20 @@
+/** @type {import("stylelint").Config} */
+const config = {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-prettier-scss',
+    'stylelint-config-property-sort-order-smacss',
+  ],
+  plugins: ['stylelint-scss'],
+  rules: {
+    'selector-class-pattern': '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+    'keyframes-name-pattern': /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/,
+    'max-nesting-depth': 3,
+    'no-descending-specificity': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'scss/at-rule-conditional-no-parentheses': null,
+  },
+};
+
+export default config;
