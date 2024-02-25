06a432c39d2fd55f82a924fc8a663a67dd5d9be5
chaejin-jen <jen71324@gmail.com>
2024-02-25 15:50:45 +0900

    feat: Added recharts sample data

---
+++ src/stories/recharts/data/sample/coloredData.ts
@@ -0,0 +1,7 @@
+const data = [
+  { name: 'A', value: 80, color: '#ff0000' },
+  { name: 'B', value: 45, color: '#00ff00' },
+  { name: 'C', value: 25, color: '#0000ff' },
+];
+
+export default data;
